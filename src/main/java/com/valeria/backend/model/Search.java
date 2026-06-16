171a6f9700e0da092bb6172ce88edbf9ead0253f
valeria <valeriacruzgarcia.7@gmail.com>
2026-06-16 17:53:00 -0600

    change 3

---
+++ src/main/java/com/valeria/backend/model/Search.java
@@ -0,0 +1,22 @@
+package com.valeria.backend.model;
+
+public class Search {
+
+    private String value;
+    private boolean case_sensitive;
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public boolean isCase_sensitive() {
+		return case_sensitive;
+	}
+	public void setCase_sensitive(boolean case_sensitive) {
+		this.case_sensitive = case_sensitive;
+	}
+
+    // getters y setters
+    
+}
---
+++ src/main/java/com/valeria/backend/model/SortRequest.java
@@ -0,0 +1,21 @@
+package com.valeria.backend.model;
+public class SortRequest {
+
+    private String field;
+    private String direction;
+	public String getField() {
+		return field;
+	}
+	public void setField(String field) {
+		this.field = field;
+	}
+	public String getDirection() {
+		return direction;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+  
+    
+}
